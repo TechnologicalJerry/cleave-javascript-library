a9d8fdbed4469fe4a6e3905f9c4961e0067ff979
technologicaljerry <cserajatverma@gmail.com>
2023-03-30 22:37:30 +0530

    Update timeFormatting.js

--- timeFormatting.js
+++ timeFormatting.js
@@ -2,3 +2,8 @@ var cleave = new Cleave('.input-element', {
     time: true,
     timePattern: ['h', 'm', 's']
 });
+
+var cleave = new Cleave('.input-element', {
+    time: true,
+    timePattern: ['h', 'm']
+});
